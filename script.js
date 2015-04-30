db0678049df72838a30e8f348f2db4b01edcfc24
maryk.december@gmail.com <maryk.december@gmail.com>
2015-04-30 08:24:15 -0500

    add emoji to md

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ $(document).ready(function() {
 	$('.day').click(function(){
 		$(this).children().toggleClass('hidden');
 	});
+
+	$('.heart').click(function(){
+		$('h5').addClass('hidden');
+		$('.moon').removeClass('hidden');
+	});
+
+    setInterval(function() {
+        $('.terminal').children().toggleClass('cya');
+    }, 800);
 });
 
 
